8829b74fcefc628d2ff3a6e43dc91778443611d6
arshan <a6Brgeuka@gmail.com>
2019-05-10 15:31:21 +0300

    run grant

--- dist/js/medium-editor-insert-plugin.js
+++ dist/js/medium-editor-insert-plugin.js
@@ -581,7 +581,7 @@ this["MediumInsert"]["Templates"]["src/js/templates/images-toolbar.hbs"] = Handl
                 }
             });
 
-            if ($p.length && (($p.text().trim() === '' && !activeAddon) || activeAddon === 'images')) {
+            if ($p.length && (!activeAddon || activeAddon === 'images')) {
                 $p.addClass('medium-insert-active');
 
                 if (activeAddon === 'images') {
@@ -1809,6 +1809,7 @@ this["MediumInsert"]["Templates"]["src/js/templates/images-toolbar.hbs"] = Handl
             $progressBarTemplate,
             progress,
             onProgress,
+            $div,
             that = this,
             uploadErrors = [],
             file = data.files[0],
@@ -1836,14 +1837,33 @@ this["MediumInsert"]["Templates"]["src/js/templates/images-toolbar.hbs"] = Handl
 
         // Replace paragraph with div, because figure elements can't be inside paragraph
         if ($place.is('p')) {
-            $place.replaceWith('<div class="medium-insert-active">' + $place.html() + '</div>');
-            $place = this.$el.find('.medium-insert-active');
-            if ($place.next().is('p')) {
-                this.core.moveCaret($place.next());
+            if ($place.text().trim()) {
+                $place.removeClass('medium-insert-active');
+                $div = $('<div>', {
+                    text: '',
+                    class: 'medium-insert-active'
+                });
+
+                $place.after($div);
+
+                $place = $div;
+                if ($place.next().is('p')) {
+                    this.core.moveCaret($place.next());
+                } else {
+                    $place.after('<p><br></p>'); // add empty paragraph so we can move the caret to the next line.
+                    this.core.moveCaret($place.next());
+                }
             } else {
-                $place.after('<p><br></p>'); // add empty paragraph so we can move the caret to the next line.
-                this.core.moveCaret($place.next());
+                $place.replaceWith('<div class="medium-insert-active">' + $place.html() + '</div>');
+                $place = this.$el.find('.medium-insert-active');
+                if ($place.next().is('p')) {
+                    this.core.moveCaret($place.next());
+                } else {
+                    $place.after('<p><br></p>'); // add empty paragraph so we can move the caret to the next line.
+                    this.core.moveCaret($place.next());
+                }
             }
+
         }
 
         $place.addClass('medium-insert-images');
